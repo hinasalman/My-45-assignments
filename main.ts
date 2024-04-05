0fdaeb8989fbfde1bfc5b526d249850c82979a1f
Hina Salman <160792823+hinasalman@users.noreply.github.com>
2024-04-05 11:21:55 -0700

    assignment 11

--- main.ts
+++ main.ts
@@ -1,31 +1,8 @@
-// Author: salman khan
+let Name : string [] = ["salman","Hina","Ayesha.Aira","Manal"];
 
-// Program 1: Storing and Printing Favorite Number
-// This program demonstrates storing and printing a favorite number.
-// Store your favorite number in a variable
-let favoriteNumber: number = 7;
+console.log(Name[0]);
+console.log(Name[1]);
+console.log(Name[2]);
+console.log(Name[3]);
 
-// Create a message that reveals your favorite number
-let favoriteMessage: string = `My favorite number is ${favoriteNumber}.`;
-
-// Print the message
-console.log(favoriteMessage);
-
-
-// Author: salman khan
-
-// Program 2: Calculating Area of a Rectangle
-// This program calculates the area of a rectangle given its width and height.
-// Define variables for width and height of the rectangle
-let width: number = 5;
-let height: number = 10;
-
-// Calculate the area of the rectangle
-let area: number = width * height;
-
-// Create a message to display the area
-let areaMessage: string = `The area of the rectangle is ${area}.`;
-
-// Print the message
-console.log(areaMessage);
 
