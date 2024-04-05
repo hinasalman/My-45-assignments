2189cdb0b65e210d2025b9d708ef531e9e7c0753
Hina Salman <160792823+hinasalman@users.noreply.github.com>
2024-04-05 11:20:45 -0700

    assignment 10

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 "use strict";
-let favoriteNumber = 4;
-console.log("My favorite number is ${favoriteNumber}.");
+// Author: salman khan
+// Program 1: Storing and Printing Favorite Number
+// This program demonstrates storing and printing a favorite number.
+// Store your favorite number in a variable
+let favoriteNumber = 7;
+// Create a message that reveals your favorite number
+let favoriteMessage = `My favorite number is ${favoriteNumber}.`;
+// Print the message
+console.log(favoriteMessage);
+// Author: salman khan
+// Program 2: Calculating Area of a Rectangle
+// This program calculates the area of a rectangle given its width and height.
+// Define variables for width and height of the rectangle
+let width = 5;
+let height = 10;
+// Calculate the area of the rectangle
+let area = width * height;
+// Create a message to display the area
+let areaMessage = `The area of the rectangle is ${area}.`;
+// Print the message
+console.log(areaMessage);
