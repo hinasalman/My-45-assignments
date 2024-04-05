dc456b12b6964ec9d4b42b23989e3cc79c704026
Hina Salman <160792823+hinasalman@users.noreply.github.com>
2024-04-05 11:50:56 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,15 @@ function show_magicians(magicians) {
         console.log(magician);
     }
 }
+// Define the make_great function
+function make_great(magicians) {
+    for (let i = 0; i < magicians.length; i++) {
+        magicians[i] = "the Great " + magicians[i];
+    }
+}
 // Array of magician's names
-const magicians = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
-// Call the function and pass the array
+let magicians = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
+// Call make_great function to modify the array
+make_great(magicians);
+// Call the show_magicians function to see the modified list
 show_magicians(magicians);
--- main.ts
+++ main.ts
@@ -5,8 +5,18 @@ function show_magicians(magicians: string[]): void {
     }
 }
 
-// Array of magician's names
-const magicians: string[] = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
+// Define the make_great function
+function make_great(magicians: string[]): void {
+    for (let i = 0; i < magicians.length; i++) {
+        magicians[i] = "the Great " + magicians[i];
+    }
+}
 
-// Call the function and pass the array
+// Array of magician's names
+let magicians: string[] = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
+
+// Call make_great function to modify the array
+make_great(magicians);
+
+// Call the show_magicians function to see the modified list
 show_magicians(magicians);
