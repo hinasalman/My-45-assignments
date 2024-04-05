5ce265e08e31ded12197c8023fd23ee171cbd84b
Hina Salman <160792823+hinasalman@users.noreply.github.com>
2024-04-05 11:26:20 -0700

    assig 16

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 "use strict";
-let guestarr = ["Anaya", "fatma", "Merab", "azal"];
-let canNotAttend = "Anaya";
-console.log(canNotAttend + "can not attend the dinner.");
-let newGuest = "Manal";
-guestarr[guestarr.indexOf(canNotAttend)] = newGuest;
+let guestarr = ["Anaya", "Fatma", "Merab"];
+let canNotAttend = "Azal";
+let newGuest = "Haddi";
 console.log(guestarr);
-guestarr.map((items) => console.log('Dear $(items), You are invited to the dinner.'));
+guestarr.map((items) => console.log('Dear ${items}, I found a bigger dinner table so i am invited more peoples.'));
+//part 2 new
+let guestNew = "Azal";
+guestarr.unshift(guestNew);
+console.log(guestarr);
+//part 3 middle
+let middleGuest = "mustafa";
+let middleIntex = guestarr.length / 3;
+guestarr.splice(middleIntex, 0, middleGuest);
+console.log(guestarr);
+//part 4 append
+guestarr.push("Abdullah");
+console.log(guestarr);
+//part 5 
+guestarr.map((items) => console.log('dear ${items}, You are invited in the more people category on dinner'));
--- main.ts
+++ main.ts
@@ -1,14 +1,34 @@
-let guestarr : string[] = ["Anaya", "fatma", "Merab", "azal"];
-let canNotAttend : string = "Anaya"
+let guestarr : string[] = ["Anaya", "Fatma", "Merab"];
 
-console.log(canNotAttend +  "can not attend the dinner.")
+let canNotAttend : string = "Azal"
 
-let newGuest : string = "Manal"
+let newGuest : string = "Haddi"
 
-guestarr [guestarr.indexOf(canNotAttend)] = newGuest;
-
-console.log(guestarr)
+console.log(guestarr);
 
 guestarr.map((items) =>
-console.log('Dear $(items), You are invited to the dinner.')
+console.log('Dear ${items}, I found a bigger dinner table so i am invited more peoples.')
 )
+//part 2 new
+let guestNew : string ="Azal"
+guestarr.unshift(guestNew);
+console.log(guestarr)
+
+//part 3 middle
+let middleGuest : string = "mustafa"
+let middleIntex = guestarr.length/3
+guestarr.splice(middleIntex,0,middleGuest)
+console.log(guestarr) 
+
+//part 4 append
+guestarr.push("Abdullah")
+console.log(guestarr)
+
+//part 5 
+guestarr.map((items)=>
+console.log('dear ${items}, You are invited in the more people category on dinner'));
+
+
+
+
+
